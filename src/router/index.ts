328a3178ad52e51a8e85f9c28a75870907d8ad57
zhubin123. <z17816012557@163.com>
2024-03-21 18:28:02 +0800

    build error

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,14 +6,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/topPage.vue')
-    },
-    {
-      path: '/vr',
-      name: 'vr',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/vR.vue')
     }
   ]
 })
